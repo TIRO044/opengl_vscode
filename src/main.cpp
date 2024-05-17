f3ce65f74d22f599f86cacacb1e661e2be0c0451
emddldans <wjdqkddl044@naver.com>
2024-05-17 09:53:18 +0900

    glad세팅

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -10,8 +11,11 @@ int main() {
     GLFWwindow* window = glfwCreateWindow(WIN_W, WIN_H, "hellow", NULL, NULL);
     glfwMakeContextCurrent(window);
 
+    glewInit();
     while(!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
