7e152d1dcf3190d31d337a7815c10de91652f44f
emddldans <wjdqkddl044@naver.com>
2024-05-18 00:27:21 +0900

    glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include <GL/glew.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "glad.c"
 
 const unsigned int WIN_W=300;
 const unsigned int WIN_H=300;
@@ -11,7 +12,7 @@ int main() {
     GLFWwindow* window = glfwCreateWindow(WIN_W, WIN_H, "hellow", NULL, NULL);
     glfwMakeContextCurrent(window);
 
-    glewInit();
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     while(!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
